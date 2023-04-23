4911614dd478c382f7a07d157f5300e6e23f3e02
Pietro De Bianchi <pietro.debianchi96@gmail.com>
2023-04-23 17:52:27 +0200

    exercise compketed :)

--- functions.php
+++ functions.php
@@ -1,9 +1,13 @@
 <?php
-
-// Read the contents of 'database.json' file and store it in a variable
-$arrayString = file_get_contents('database.json');
-// Decode the JSON data and store it in an associative array
-$listToDo = json_decode($arrayString, true);
+// check if the file database.json exists
+if (file_exists('database.json')) {
+    // Read the contents of 'database.json' file and store it in a variable
+    $arrayString = file_get_contents('database.json');
+    // Decode the JSON data and store it in an associative array
+    $listToDo = json_decode($arrayString, true);
+} else {
+    $listToDo = [];
+}
 
 // Check if a POST request parameter 'tasking' is set
 if (isset($_POST['tasking'])) {
@@ -17,7 +21,24 @@ if (isset($_POST['tasking'])) {
     $dataString = json_encode($listToDo);
     // Write the updated data back to 'database.json' file
     file_put_contents('database.json', $dataString);
-};
+} elseif (isset($_POST['remove'])) { // Check if a POST request parameter 'remove' is set
+    // Get the index of the task to be removed from the POST data
+    $index = $_POST['remove'];
+
+    // Remove the task from the $listToDo array based on the index
+    if (isset($listToDo[$index])) {
+        array_splice($listToDo, $index, 1);
+        // Encode the updated $listToDo array back to JSON format
+        $dataString = json_encode($listToDo);
+        // Write the updated data back to 'database.json' file
+        file_put_contents('database.json', $dataString);
+        // Set the response header to indicate JSON format
+        header("Content-Type: application/json");
+        // Encode the $listToDo array back to JSON format and send as response
+        echo json_encode($listToDo);
+        exit(); // Exit the script after sending response
+    }
+}
 
 // Set the response header to indicate JSON format
 header("Content-Type: application/json");
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { createApp } = Vue
+const { createApp } = Vue;
 
 createApp({
     data() {
@@ -11,40 +11,61 @@ createApp({
         }
     },
     methods: {
+        // Fetch list of tasks from server
         checkList() {
             axios.get('functions.php')
                 .then(response => {
-                    console.log(response)
+                    console.log(response);
                     this.listToDo = response.data;
                 })
+                .catch(error => {
+                    console.error('Failed to fetch list of tasks:', error);
+                });
         },
+        // Submit new task to server
         submit() {
             const data = {
                 tasking: this.newTask
             };
 
-            axios.post('functions.php', data,
-                {
+            axios.post('functions.php', data, {
                     headers: { 'Content-Type': 'multipart/form-data' }
                 })
                 .then(response => {
                     this.listToDo = response.data;
                     this.newTask.task = '';
                 })
+                .catch(error => {
+                    console.error('Failed to submit task:', error);
+                });
         },
-        toogleClass(todo) {
+        removeTask(index) {
+            // Send POST request to server with task index as parameter
+            const data = {
+            remove: index
+            };
+            axios.post('functions.php', data, {
+                headers: { 'Content-Type': 'multipart/form-data' }
+            })
+            .then(response => {
+                // Update list of tasks in Vue component
+                this.listToDo = response.data;
+            })
+            .catch(error => {
+                console.error('Failed to remove task:', error);
+            });
+        },
+        // Toggle task's done status
+        toggleClass(todo) {
             if (todo.done == true) {
-
                 todo.done = false;
             } else {
                 todo.done = true;
             }
         },
-        removeTask(index) {
-            this.listToDo.splice(index, 1);
-        },
     },
     mounted() {
+        // Fetch initial list of tasks when component is mounted
         this.checkList();
     }
 }).mount('#app');
