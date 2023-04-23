78f77aa683be1b25e4cad18bdb8ac9d22affe664
Pietro De Bianchi <pietro.debianchi96@gmail.com>
2023-04-23 17:06:45 +0200

    first part of json database done!

--- functions.php
+++ functions.php
@@ -1,34 +1,26 @@
 <?php
 
-$listToDo = [
-    [
-        "task" => "HTML",
-        "done" => true,
-    ],
-    [
-        "task" => "CSS",
-        "done" => true,
-    ],
-    [
-        "task" => "JS",
-        "done" => true,
-    ],
-    [
-        "task" => "PHP",
-        "done" => false,
-    ],
-    [
-        "task" => "LARAVEL",
-        "done" => false,
-    ],
-];
+// Read the contents of 'database.json' file and store it in a variable
+$arrayString = file_get_contents('database.json');
+// Decode the JSON data and store it in an associative array
+$listToDo = json_decode($arrayString, true);
 
+// Check if a POST request parameter 'tasking' is set
 if (isset($_POST['tasking'])) {
+    // Append a new element to the $listToDo array
     $listToDo[] = [
         'task' => $_POST['tasking']['task'],
         'done' => $_POST['tasking']['done'] === "false" ? false : true
     ];
+
+    // Encode the updated $listToDo array back to JSON format
+    $dataString = json_encode($listToDo);
+    // Write the updated data back to 'database.json' file
+    file_put_contents('database.json', $dataString);
 };
 
+// Set the response header to indicate JSON format
 header("Content-Type: application/json");
+// Encode the $listToDo array back to JSON format and send as response
 echo json_encode($listToDo);
+?>
